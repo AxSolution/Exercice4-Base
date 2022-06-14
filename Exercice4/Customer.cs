3060a2e20571f876ad66e28663eedba756452c65
Lucas Dupouy <dupouy.lucas@gmail.com>
2022-06-14 11:04:43 +0200

    Indentation fix

--- Exercice4/Customer.cs
+++ Exercice4/Customer.cs
@@ -6,47 +6,47 @@ using System.Threading.Tasks;
 
 namespace Exercice4
 {
-	public class Customer : ICustomer
-	{
-		string cid;
-		string name;
-		public Customer(string cid, string name)
-		{
-			uint i = 0;
-			if (uint.TryParse(cid, out i))
-			{
-				this.cid = cid;
-			}
+    public class Customer : ICustomer
+    {
+        string cid;
+        string name;
+        public Customer(string cid, string name)
+        {
+            uint i = 0;
+            if (uint.TryParse(cid, out i))
+            {
+                this.cid = cid;
+            }
             else
             {
-				throw new BadIDException();
+                throw new BadIDException();
             }
-			this.name = name;
-		}
+            this.name = name;
+        }
 
-		int IComparable<ICustomer>.CompareTo(ICustomer other)
-		{
-			throw new NotImplementedException();
-		}
+        int IComparable<ICustomer>.CompareTo(ICustomer other)
+        {
+            throw new NotImplementedException();
+        }
 
-		bool IEquatable<ICustomer>.Equals(ICustomer other)
-		{
-			throw new NotImplementedException();
-		}
+        bool IEquatable<ICustomer>.Equals(ICustomer other)
+        {
+            throw new NotImplementedException();
+        }
 
-		string ICustomer.GetCID()
-		{
-			return cid;
-		}
+        string ICustomer.GetCID()
+        {
+            return cid;
+        }
 
-		string IPerson.GetName()
-		{
-			return name;
-		}
+        string IPerson.GetName()
+        {
+            return name;
+        }
 
-		void IPrintable.Print(IPrinter printer)
-		{
-			Console.WriteLine("cid: " + this.cid + " - name: " + this.name);
-		}
-	}
+        void IPrintable.Print(IPrinter printer)
+        {
+            Console.WriteLine("cid: " + this.cid + " - name: " + this.name);
+        }
+    }
 }
